2eafabcf56de5caa6c6c1886b58bb8fd6678be1f
lihongyi <lihongyi@deepexi.com>
2019-06-29 18:01:06 +0800

    add first

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // 将确保你的单行代码不会超过 100 个字符
+  printWidth: 100,
+  // 会将所有双引号转换为单引号
+  singleQuote: true,
+  // 将确保在最后一个对象属性的末尾会有一个逗号
+  trailingComma: 'all',
+  // 在对象字面量之间打印空格
+  bracketSpacing: true,
+  // jsxBracketSameLine 将在多行 JSX 元素的最后一行放置 >
+  jsxBracketSameLine: false,
+  // 指定单个缩进的空格数
+  tabWidth: 2,
+  // 将在语句末尾加上 ;
+  semi: true,
+};
