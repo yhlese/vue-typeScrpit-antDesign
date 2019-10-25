dccf15be1d77cd997958975c8d9e1d65e7d6f387
yhlese <yhlese@qq.com>
2019-10-25 16:14:40 +0800

    feat：proxy

--- src/doc/vue.3.0.js
+++ src/doc/vue.3.0.js
@@ -1,8 +1,8 @@
 /**
- * @description 
+ * @description
  * vue2.0 : defineProperty
  * vue3.0 ：proxy
- * 
+ *
  */
 
 /**
@@ -15,26 +15,26 @@
 var a = {
   b: 123,
 };
-Object.defineProperty (a, 'b', {
-  set: function (newVal) {
-    console.log ('i am be set');
+Object.defineProperty(a, 'b', {
+  set: function(newVal) {
+    console.log('i am be set');
   },
 });
 
 /**
  * @description vue3.0
- * 
+ *
  * 原理：proxy 是在源数据进行一层拦截 new 了一个新的对象，对新对象进行设置时， 才会去触发 set 方法，
  * 优点：1. 避免了对源数据的污染。（修改源数据是不会触发 set 方法）
  *      2. 而且是对整个对象的监听，避免了无数次属性的绑定监听
  *      3. 在 set 方法中只能拿到 （ 源对象，改变的属性key，改变的新值 ）
- *  
+ *
  */
 var a1 = {
   b: 234,
 };
-var newA = new Proxy (a, {
-  set: function (target, key, newVal) {
-    console.log ('i am be set');
+var newA = new Proxy(a, {
+  set: function(target, key, newVal) {
+    console.log('i am be set');
   },
 });
